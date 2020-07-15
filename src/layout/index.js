a784962a11d446c4ff16731d2f96bbed19fcec98
Sebastien Caietta <sebastien.caietta@gmail.com>
2020-07-15 22:46:52 +0100

    Keep footer at the bottom of the page at all times

--- src/layout/index.js
+++ src/layout/index.js
@@ -8,7 +8,7 @@ import Footer from "./footer";
 const useStyles = makeStyles(theme => ({
     content: {
         flexGrow: 1,
-        paddingTop: theme.spacing(3),
+        padding: theme.spacing(3, 0),
         position: 'relative',
     },
 }));
